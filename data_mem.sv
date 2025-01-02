bcb1e7e14272934c2e2b0eaa0984ce22208fea0c
mohammedkhaledalsaedi <mohammad.k.2372@gmail.com>
2025-01-02 10:50:04 +0300

    Update data_mem.sv

--- data_mem.sv
+++ data_mem.sv
@@ -17,20 +17,34 @@ output logic [31:0]rdata //read data
                               // in the following:
     
     always_comb begin
-        case (func3[1:0])
-        2'b000 : case (addr[1:0])
-            'b00 : rdata = dmem[addr[31:2]] [7:0]; //first byte of nth word location 
-            'b01 : rdata = dmem[addr[31:2]] [15:8]; //second byte of nth word location 
-            'b10 : rdata = dmem[addr[31:2]] [23:16]; //third byte of nth word location 
-            'b11 : rdata = dmem[addr[31:2]] [31:24]; //fourth byte of nth word location 
+        case (func3[2:0])
+        3'b000 : case (addr[1:0])
+            'b00 : rdata = {{24{dmem[addr[31:2]][7]}}, dmem[addr[31:2]][7:0]}; //first byte of nth word location 
+            'b01 : rdata = {{24{dmem[addr[31:2]][15]}}, dmem[addr[31:2]][15:8]}; //second byte of nth word location 
+            'b10 : rdata = {{24{dmem[addr[31:2]][23]}}, dmem[addr[31:2]][23:16]}; //third byte of nth word location 
+            'b11 : rdata = {{24{dmem[addr[31:2]][31]}}, dmem[addr[31:2]][31:24]}; //fourth byte of nth word location 
         endcase
         
-        2'b001 : case (addr[1])
-            'b0 : rdata = dmem[addr[31:2]] [15:0]; //first half (hw) of nth word location 
-            'b1 : rdata = dmem[addr[31:2]] [31:16]; //second half (hw) of nth word location 
+        3'b001 : case (addr[1])
+            'b0 : rdata = {{16{dmem[addr[31:2]][15]}}, dmem[addr[31:2]][15:0]}; //first half (hw) of nth word location 
+            'b1 : rdata = {{16{dmem[addr[31:2]][31]}}, dmem[addr[31:2]][31:16]}; //second half (hw) of nth word location 
         endcase
         
-        2'b010 : rdata = dmem[addr[31:2]]; //word addressable
+        3'b010 : rdata = dmem[addr[31:2]]; //word addressable
+     // -----------------------------------------------------------------------------
+        3'b100 : case (addr[1:0]) // ---------- LBU
+            'b00 : rdata = {24'b0, dmem[addr[31:2]][7:0]}; //first byte of nth word location 
+            'b01 : rdata = {24'b0, dmem[addr[31:2]][15:8]}; //second byte of nth word location 
+            'b10 : rdata = {24'b0, dmem[addr[31:2]][23:16]}; //third byte of nth word location 
+            'b11 : rdata = {24'b0, dmem[addr[31:2]][31:24]}; //fourth byte of nth word location 
+        endcase
+        
+        3'b101 : case (addr[1]) // ---------- LHU
+            'b0 : rdata = {16'b0, dmem[addr[31:2]][15:0]}; //first half (hw) of nth word location 
+            'b1 : rdata = {16'b0, dmem[addr[31:2]][31:16]}; //second half (hw) of nth word location 
+        endcase
+        
+        default: rdata = 32'b0;
         
         endcase
     end
@@ -45,21 +59,39 @@ output logic [31:0]rdata //read data
         else if(mem_write) begin
         
         case (func3[1:0])
-        2'b000 : case (addr[1:0])
+        'b00 : case (addr[1:0])
             'b00 : dmem[addr[31:2]] [7:0] <= wdata[7:0];
             'b01 : dmem[addr[31:2]] [15:8] <= wdata[7:0];
             'b10 : dmem[addr[31:2]] [23:16] <= wdata[7:0];
             'b11 : dmem[addr[31:2]] [31:24] <= wdata[7:0];
             endcase
         
-        2'b001 : case (addr[1])
+        'b01 : case (addr[1])
             'b0 : dmem[addr[31:2]] [15:0] <= wdata[15:0];
             'b1 : dmem[addr[31:2]] [31:16] <= wdata[15:0];
             endcase
         
-        2'b010 : dmem[addr[31:2]] [31:0] <= wdata; //wdata in default is wdata[31:0]
+        'b10 : dmem[addr[31:2]] [31:0] <= wdata; //wdata in default is wdata[31:0]
         
         endcase
+        
+//        case (func3[2:0])
+//        'b100 : case (addr[1:0])
+//            'b00 : dmem[addr[31:2]] [7:0] <= wdata[7:0];
+//            'b01 : dmem[addr[31:2]] [15:8] <= wdata[7:0];
+//            'b10 : dmem[addr[31:2]] [23:16] <= wdata[7:0];
+//            'b11 : dmem[addr[31:2]] [31:24] <= wdata[7:0];
+//            endcase
+        
+//        'b101 : case (addr[1])
+//            'b0 : dmem[addr[31:2]] [15:0] <= wdata[15:0];
+//            'b1 : dmem[addr[31:2]] [31:16] <= wdata[15:0];
+//            endcase
+        
+//        'b110 : dmem[addr[31:2]] [31:0] <= wdata; //wdata in default is wdata[31:0]
+        
+//        endcase
+        
         end
     
     
